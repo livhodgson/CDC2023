17956f583d10bf02f5ac5c44c875c926190f7f71
kevics <111520331+kevics@users.noreply.github.com>
2023-09-30 18:09:09 -0400

    Added EDA for fire size class based on report availability

--- CDC Code.R
+++ CDC Code.R
@@ -22,3 +22,44 @@ practice3
 # `ICS_209_INCIDENT_NUMBER` N=14561
 # `MTBS_ID` N=5474
 
+fires_id_code = fires |> 
+  mutate(ics_yn = ifelse(ICS_209_INCIDENT_NUMBER != "", T, F),
+         mtbs_yn = ifelse(MTBS_ID != "", T, F)) |> 
+  group_by(ICS_209_INCIDENT_NUMBER) |> 
+  mutate(ics_ct = sum(ics_yn),
+         ics_fire_sum = sum(FIRE_SIZE)) |> 
+  ungroup() |> 
+  group_by(MTBS_ID) |> 
+  mutate(mtbs_ct = sum(mtbs_yn),
+         mtbs_fire_sum = sum(FIRE_SIZE)) |> 
+  ungroup()
+  # select(ics_ct) |> 
+  # arrange(desc(ics_ct)) |> head(10)
+
+fires_id_code |> 
+  filter(ics_ct != 0) |> 
+  ggplot(aes(x = ics_ct, y = ics_fire_sum)) + geom_point()
+
+fires_id_code |> 
+  filter(mtbs_ct != 0) |> 
+  ggplot(aes(x = mtbs_ct, y = mtbs_fire_sum)) + geom_point()
+
+fires_id_code |> 
+  filter(ics_ct == 0) |> 
+  ggplot(aes(x = FIRE_SIZE_CLASS)) + geom_bar()
+
+fires_id_code |> 
+  filter(ics_ct != 0) |> 
+  ggplot(aes(x = FIRE_SIZE_CLASS)) + geom_bar()
+
+
+fires_id_code |> 
+  filter(mtbs_ct == 0) |> 
+  ggplot(aes(x = FIRE_SIZE_CLASS)) + geom_bar()
+
+fires_id_code |> 
+  filter(mtbs_ct != 0) |> 
+  ggplot(aes(x = FIRE_SIZE_CLASS)) + geom_bar()
+
+## If there is an MTBS or ICS report, the fires tend to be larger (higher size class)
+
