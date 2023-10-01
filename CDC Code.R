25580d12268b9211f04c8555d59487bce63551f8
kevics <111520331+kevics@users.noreply.github.com>
2023-09-30 20:56:03 -0400

    just some more EDA

--- CDC Code.R
+++ CDC Code.R
@@ -1,8 +1,14 @@
 library('tidyverse')
+library(lubridate)
 
+state_region = read.csv("/Users/kevinsu/Downloads/us census bureau regions and divisions.csv")
 
-fires = read.csv('fires.csv')
+fires = read.csv('fires.csv') |> 
+  #mutate(FIRE_YEAR = lubridate::year(lubridate::mdy(FIRE_YEAR))) |> 
+  inner_join(state_region, by = c("STATE" = "State.Code"))
 head(fires)
+
+
 
 fires_cln = fires[fires$STAT_CAUSE_DESCR != c("Miscellaneous", "Missing/Undefined"),]
 practice = ggplot(fires, aes(x = FIRE_SIZE, y = FIRE_YEAR, color = STAT_CAUSE_DESCR)) + geom_point()
@@ -44,6 +50,7 @@ fires_id_code |>
   filter(mtbs_ct != 0) |> 
   ggplot(aes(x = mtbs_ct, y = mtbs_fire_sum)) + geom_point()
 
+# ICS report
 fires_id_code |> 
   filter(ics_ct == 0) |> 
   ggplot(aes(x = FIRE_SIZE_CLASS)) + geom_bar()
@@ -52,7 +59,7 @@ fires_id_code |>
   filter(ics_ct != 0) |> 
   ggplot(aes(x = FIRE_SIZE_CLASS)) + geom_bar()
 
-
+# MTBS report
 fires_id_code |> 
   filter(mtbs_ct == 0) |> 
   ggplot(aes(x = FIRE_SIZE_CLASS)) + geom_bar()
@@ -63,3 +70,30 @@ fires_id_code |>
 
 ## If there is an MTBS or ICS report, the fires tend to be larger (higher size class)
 
+# Exploratory Analysis
+fires |> 
+  ggplot(aes(x = FIRE_SIZE_CLASS)) + geom_bar() + facet_wrap(~STAT_CAUSE_DESCR) + coord_flip()
+
+fires |> 
+  ggplot(aes(x = OWNER_DESCR)) + geom_bar() + facet_wrap(~FIRE_SIZE_CLASS) + coord_flip()
+
+fires |> 
+  group_by(STAT_CAUSE_DESCR) |> 
+  summarize(max_fire = max(FIRE_SIZE)) |> 
+  arrange(desc(max_fire))
+
+fires |> 
+  group_by(FIRE_YEAR, Region) |> 
+  mutate(max_fire_yr = max(FIRE_SIZE)) |> 
+  ggplot(aes(x=FIRE_YEAR, y = max_fire_yr, color =Region)) + geom_line() + theme_minimal() + 
+  scale_x_continuous(limits = c(2000,2010), n.breaks = 9, minor_breaks = NULL)
+
+fires |> 
+  ggplot(aes(y = CONT_TIME, x = FIRE_SIZE, color = Region)) + geom_point()
+
+
+fires |> 
+  group_by(STAT_CAUSE_DESCR, Region) |> 
+  summarize(median = median(CONT_TIME,na.rm=T)) |> 
+  ggplot(aes(y = STAT_CAUSE_DESCR, x = Region, fill = median)) + geom_tile() + 
+  geom_text(aes(label = median), color = "white", size = 4) + theme_classic()
